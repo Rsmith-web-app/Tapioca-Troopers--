fa420f719394badd616183b33cc431482b18336c
CodeyOwl <codeyowl30@gmail.com>
2024-11-12 01:23:32 -0500

    added mongo db conenction logic

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const {MongoClient, ServerApiVersion, version} = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const connectionString = process.env.MONGO_DB_CONNECTION_STRING;
+
+
+//Mongo Client setup
+const client = MongoClient(connectionString, {
+    ServerApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    }
+});
+
+//connect to MongoDb
+const MongoConnect = async ()  => {
+    try {
+        client.connect();
+        console.log('conneced to MongoDB')
+    } catch (error) {
+        console.log(`the following error occured: ${error}`)
+    }
+}
 
 //Insert MongoDB Connection - Dani Baker
-
-
-
+app.set('view engine', 'ejs');
 //add mongodb connection logic
 
 
