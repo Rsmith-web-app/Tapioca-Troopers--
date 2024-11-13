cc6f19cd994809ace1eb58d10df210c5bb8a9f36
Mo'ath Zaghdad <moath.m.zaghdad@gmail.com>
2024-11-13 10:12:42 -0500

    feat: Add pages templates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Blog from './pages/Blog';
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Blog />,
+  },
+  {
+    path: "/sign-in",
+    element: <SignIn />
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp />
+  }
+]);
 
 function App() {
   return (
     <>
-      <h1>App</h1>
+      <RouterProvider router={router} />
     </>
   );
 }
