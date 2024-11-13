8efb3ef109ba6c81e43dc8488afcce476c55c5c6
CodeyOwl <codeyowl30@gmail.com>
2024-11-13 03:15:52 -0500

    Update

--- backend/models/post.model.js.js
+++ backend/models/post.model.js.js
@@ -6,6 +6,7 @@ const postSchema = new Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  comments: {type: mongoose.Schema.Types.ObjectId, ref: 'comments', required: true},
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
