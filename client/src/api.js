651e8f5a74d5e1074e126a17929997d2a022f8c8
Mo'ath Zaghdad <moath.m.zaghdad@gmail.com>
2024-12-09 13:17:09 -0500

    WIP: add SERVER_URL ENV

--- client/src/api.js
+++ client/src/api.js
@@ -1,8 +1,34 @@
 import axios from 'axios';
 
+const serverUrl = process.env.SERVER_URL || 'http://localhost:3060';
+console.warn(serverUrl);
 
 const getPostsApi = async () => {
-  const response = await axios.get('http://localhost:3060/api/post/');
+  const response = await axios.get(`${serverUrl}/api/post/`);
   return response.data.posts;
 }
-export { getPostsApi };
+
+
+const userLogin = async (formData) => {
+  const resp = await fetch(`${serverUrl}/api/login`, {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+  return resp;
+}
+
+
+const userRegister = async (formData) => {
+  const resp = await fetch("http://localhost:3060/api/register", {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+}
+
+export { getPostsApi, userLogin, userRegister };
