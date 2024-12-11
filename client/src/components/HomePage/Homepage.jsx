804ae58a452981f3b90ea8783c1ea8581bf4017a
Mo'ath Zaghdad <moath.m.zaghdad@gmail.com>
2024-12-10 23:19:09 -0500

    fix: correct author name access & add spinner to home page
    - Fixed an issue where the author name was not accessed correctly.
    - Added a spinner on the home page to improve UX when the posts API is slow.

--- client/src/components/HomePage/Homepage.jsx
+++ client/src/components/HomePage/Homepage.jsx
@@ -3,8 +3,10 @@ import { Box, Typography, Container } from "@mui/material";
 import HomeContent from "./HomeContent";
 import Navbar from "../Navbar/navbar";
 import { getPostsApi } from "../../api";
+import { CircularProgress } from '@mui/material';
 
 export default function HomePage() {
+  const [loading, setLoading] = useState(true);
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
@@ -14,6 +16,7 @@ export default function HomePage() {
         (a, b) => new Date(b.updatedAt) - new Date(a.updatedAt)
       );
       setPosts(sortedPosts.slice(0, 4));
+      setLoading(false);
     };
 
     fetchPosts();
@@ -21,56 +24,61 @@ export default function HomePage() {
 
   return (
     <>
-     <Navbar />
-    
-    <Box
-      sx={{
-        minHeight: "150vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-      }}
-    >
-     
-      <Container>
-        <Typography
-          variant="h4"
-          align="center"
-          style={{ marginBottom: "32px", marginTop: "50px" }}
-        >
-          Hi, welcome to the Tapioca Troopers blog site.
-        </Typography>
-        <Typography variant="h6" align="center" style={{ marginBottom: "70px" }}>
-          <h4>
-            Below is a guide to each tab you can access through the menu bar, as
-            well as the most recent posts!
-          </h4>
-        </Typography>
+      <Navbar />
 
-        <Box sx={{ display: "flex", flexDirection: "row", gap: 4 }}>
-          <Box sx={{ flex: 1, maxWidth: "900px" }}>
-            <HomeContent topPosts={posts} />
+      <Box
+        sx={{
+          minHeight: "150vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+
+        <Container>
+          <Typography
+            variant="h4"
+            align="center"
+            style={{ marginBottom: "32px", marginTop: "50px" }}
+          >
+            Hi, welcome to the Tapioca Troopers blog site.
+          </Typography>
+          <Typography variant="h4" align="center" style={{ marginBottom: "70px" }}>
+            <h4>
+              Below is a guide to each tab you can access through the menu bar, as
+              well as the most recent posts!
+            </h4>
+          </Typography>
+
+          <Box sx={{ display: "flex", flexDirection: "row", gap: 4 }}>
+            <Box sx={{ flex: 1, maxWidth: "900px" }}>
+              {loading ? (
+                <CircularProgress color="secondary" />
+              ) : (
+                <HomeContent topPosts={posts} />
+              )}
+
+            </Box>
+            <Box sx={{ flex: 1, maxWidth: "400px" }}>
+              <br />
+              <ul>
+                <li style={{ marginBottom: "24px" }}>
+                  <b>Home:</b> View the latest blog posts and updates.
+                </li>
+                <li style={{ marginBottom: "24px" }}>
+                  <b>About Us:</b> Learn more about the Tapioca Troopers team.
+                </li>
+                <li style={{ marginBottom: "24px" }}>
+                  <b>Contact:</b> Get in touch with us for inquiries or feedback.
+                </li>
+                <li style={{ marginBottom: "24px" }}>
+                  <b>Archives:</b> Browse older posts and content.
+                </li>
+              </ul>
+            </Box>
           </Box>
-          <Box sx={{ flex: 1, maxWidth: "400px" }}>
-            <br />
-            <ul>
-              <li style={{ marginBottom: "24px" }}>
-                <b>Home:</b> View the latest blog posts and updates.
-              </li>
-              <li style={{ marginBottom: "24px" }}>
-                <b>About Us:</b> Learn more about the Tapioca Troopers team.
-              </li>
-              <li style={{ marginBottom: "24px" }}>
-                <b>Contact:</b> Get in touch with us for inquiries or feedback.
-              </li>
-              <li style={{ marginBottom: "24px" }}>
-                <b>Archives:</b> Browse older posts and content.
-              </li>
-            </ul>
-          </Box>
-        </Box>
-      </Container>
-    </Box>
+        </Container>
+      </Box>
     </>
   );
 }
